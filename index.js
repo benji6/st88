0620b51bf792264d6715817b455345ea6994ae5d
Ben Hall <benji6@users.noreply.github.com>
2016-04-14 22:54:30 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,29 @@
-export default ((listeners = []) => state => ({
-  dispatch(f) {
+const listeners = []
+
+module.exports = state => ({
+  dispatch: function (f) {
     state = f(state)
-    for (let i = 0; i < listeners.length; i++) listeners[i]()
+    for (var i = 0; i < listeners.length; i++) listeners[i]()
   },
 
-  getState() {
+  getState: function () {
     return state
   },
 
-  subscribe(listener) {
+  subscribe: function (listener) {
     if (typeof listener !== 'function') {
       throw new Error('Expected listener to be a function.')
     }
 
+    var isSubscribed = true
+
     listeners.push(listener)
 
-    let isSubscribed = true
-    return () => {
+
+    return function () {
       if (!isSubscribed) return
       isSubscribed = false
       listeners.splice(listeners.indexOf(listener), 1)
     }
   }
-}))()
+})
